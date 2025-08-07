7b8c960ba2415af0c28219d95f54d93d4c207ec7
excitedplus1s <github@msfconsole.cn>
2025-08-08 06:53:35 +0800

    ...

--- fetch.sh
+++ fetch.sh
@@ -8,14 +8,14 @@ for domain in $DOMAINS; do
     for subnet in $SUBNETS; do
         case $subnet in
         *":"*) 
-            dns=$(awk 'BEGIN{srand();} {print rand(), $0;}' dns6.txt | sort -k1,1n | head -n 1 | awk '{print $2}')
-            dig $domain @$dns -t AAAA +subnet=$subnet +short >> $domain/ipv6.txt
+            dns=$(awk 'BEGIN{srand();} {print rand(), $0;}' dns4.txt | sort -k1,1n | head -n 1 | awk '{print $2}')
+            dig $domain @$dns -t AAAA +subnet=$subnet +short 2>/dev/null >> $domain/ipv6.txt
             cat $domain/ipv6.txt | sort |uniq > $domain/ipv6_u.txt
             mv $domain/ipv6_u.txt $domain/ipv6.txt
             ;;
         *)
             dns=$(awk 'BEGIN{srand();} {print rand(), $0;}' dns4.txt | sort -k1,1n | head -n 1 | awk '{print $2}')
-            dig $domain @$dns -t A +subnet=$subnet +short >> $domain/ipv4.txt
+            dig $domain @$dns -t A +subnet=$subnet +short 2>/dev/null >> $domain/ipv4.txt
             cat $domain/ipv4.txt | sort |uniq > $domain/ipv4_u.txt
             mv $domain/ipv4_u.txt $domain/ipv4.txt
             ;;
@@ -25,5 +25,5 @@ done
 
 cp README_template.md README.md
 for domain in $DOMAINS; do
-    echo "[$domain]($domain)" >> README.md
+    echo "[$domain]($domain)\n" >> README.md
 done
