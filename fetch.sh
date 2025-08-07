fcf1661ac76cd377318abdf06f67d3099e81e50a
excitedplus1s <github@msfconsole.cn>
2025-08-07 21:06:04 +0800

    ...

---
+++ fetch.sh
@@ -0,0 +1,31 @@
+#! /usr/bin/env sh
+curl https://api.cloudflare.com/local-ip-ranges.csv -o local-ip-ranges.csv
+SUBNETS=$(cat local-ip-ranges.csv | awk -F, '{print $1}')
+rm -f local-ip-ranges.csv
+DOMAINS=$(cat domain.txt)
+for domain in $DOMAINS; do
+    mkdir -p $domain
+    touch $domain/ipv4.txt
+    touch $domain/ipv6.txt
+    for subnet in $SUBNETS; do
+        case $subnet in
+        *":"*) 
+            dns=$(awk 'BEGIN{srand();} {print rand(), $0;}' dns6.txt | sort -k1,1n | head -n 1 | awk '{print $2}')
+            dig $domain @$dns -t AAAA +subnet=$subnet +short >> $domain/ipv6.txt
+            cat $domain/ipv6.txt | sort |uniq > $domain/ipv6_u.txt
+            mv $domain/ipv6_u.txt $domain/ipv6.txt
+            ;;
+        *)
+            dns=$(awk 'BEGIN{srand();} {print rand(), $0;}' dns4.txt | sort -k1,1n | head -n 1 | awk '{print $2}')
+            dig $domain @$dns -t A +subnet=$subnet +short >> $domain/ipv4.txt
+            cat $domain/ipv4.txt | sort |uniq > $domain/ipv4_u.txt
+            mv $domain/ipv4_u.txt $domain/ipv4.txt
+            ;;
+        esac
+    done
+done
+
+cp README_template.md README.md
+for domain in $DOMAINS; do
+    echo "[$domain]($domain)" >> README.md
+done
